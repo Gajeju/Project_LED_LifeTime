485486249bfb02099af9e1eb3a54e3e15df0eb74
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-05-24 12:35:33 +0900

    가장 적게 사용된 LED를 찾는 알고리즘 설계 (3개중 2개만 사용한다고 가정)

---
+++ find_best_lifetime_LED.ino
@@ -0,0 +1,71 @@
+int led1 = 3;
+int led2 = 4;
+int led3 = 5;
+int sw = 12;
+
+int time[3] = {0, 1, 2};
+int temp[3];
+
+int index1;
+int index2;
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(sw, INPUT_PULLUP);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if (digitalRead(sw) == LOW)   //스위치 on
+  {
+    digitalWrite (index1 + 3, HIGH);
+    time[index1] += 1;
+    digitalWrite (index2 + 3, HIGH);
+    time[index2] += 1;
+    delay(100);
+  }
+  else                        //스위치 off
+  {
+    digitalWrite (led1, LOW);
+    digitalWrite (led2, LOW);
+    digitalWrite (led3, LOW);
+
+//배열 복사
+    for (int i = 0; i < 3; i++)
+    {
+      temp[i] = time[i];
+    }
+
+//오름차순 정렬
+    for (int i = 0; i < 3; i++)
+    {
+    for (int j = 0; j < 3-(i+1); j++)
+      if (temp[j] >= temp[j+1])
+      {
+        int m = temp[j+1];
+        temp[j+1] = temp[j];
+        temp[j] = m;
+      }
+    }
+    
+
+//index 초기화
+    for (int i = 0; i < 3; i++)
+    {
+      if (time[i] == temp[0]) index1 = i;
+      if (time[i] == temp[1]) index2 = i;
+    }
+  }
+
+ Serial.println(temp[0]);
+ Serial.println(temp[1]);
+ Serial.println(temp[2]);
+ Serial.println(" ");
+
+// Serial.println(index1);
+// Serial.println(index2);
+}
