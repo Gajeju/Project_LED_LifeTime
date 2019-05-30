d93c04a32e63083bec147e039fa2c9dda13120b2
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-05-30 14:32:03 +0900

    아두이노 내장 EEPROM 를 활용하여 시간데이터 보존

--- Lamp_with_switch.ino
+++ Lamp_with_switch.ino
@@ -1,39 +1,87 @@
+#include <EEPROM.h>
+
 int led1 = 3;
 int led2 = 4;
 int led3 = 5;
 int sw = 12;
 
-int time[3] = {0, 0, 0};
+int time[3] = {0, 1, 2};
 
-int index1 = 3;
-int index2 = 4;
+int temp[3];
+int index1;
+int index2;
 
 void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(sw, INPUT_PULLUP);
+
+  Serial.begin(9600);
+
+  if (EEPROM.read(0))
+  {
+    time[0] = EEPROM.read(0);
+    time[1] = EEPROM.read(1);
+    time[2] = EEPROM.read(2);
+  }
 }
 
 void loop() {
 
-  if (digitalRead(sw) == HIGH)
+  if (digitalRead(sw) == LOW)   //스위치 on
   {
-    digitalWrite (index1, HIGH);
-    time[index1 - 3] += 1;
-    digitalWrite (index2, HIGH);
-    time[index2 - 2] += 1;
-    delay(300);
+    digitalWrite (index1 + 3, HIGH);
+    time[index1] += 1;
+    digitalWrite (index2 + 3, HIGH);
+    time[index2] += 1;
+    delay(100);
+  }
+  else                        //스위치 off
+  {
+    digitalWrite (led1, LOW);
+    digitalWrite (led2, LOW);
+    digitalWrite (led3, LOW);
 
-    if (time[index1 - 3] == 3)
+    //배열 복사
+    for (int i = 0; i < 3; i++)
     {
-      digitalWrite (index1, LOW);
-      digitalWrite (index2, LOW);
-      index1++;
-      index2++;
-      if (index1 == 6) index1 = 3;
-      if (index2 == 6) index2 = 3;
-      time[index1 - 3] = 0;
+      temp[i] = time[i];
+    }
+
+    //오름차순 정렬
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 3 - (i + 1); j++)
+        if (temp[j] >= temp[j + 1])
+        {
+          int m = temp[j + 1];
+          temp[j + 1] = temp[j];
+          temp[j] = m;
+        }
+    }
+
+    //index 초기화
+    for (int i = 0; i < 3; i++)
+    {
+      if (time[i] == temp[0]) index1 = i;
+      if (time[i] == temp[1]) index2 = i;
+    }
+  }
+
+  Serial.println(temp[0]);
+  Serial.println(temp[1]);
+  Serial.println(temp[2]);
+  Serial.println(" ");
+
+  EEPROM.write(0, time[0]);
+  EEPROM.write(1, time[1]);
+  EEPROM.write(2, time[2]);
+
+  if (EEPROM.read(0) > 200)
+  {
+    for (int i = 0 ; i < EEPROM.length() ; i++) {
+      EEPROM.write(i, 0);
     }
   }
 }
