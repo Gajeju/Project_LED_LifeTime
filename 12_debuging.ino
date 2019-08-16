0cffa178d333517a95f67bde2bc2f6c7e4ea0cf5
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-08-16 14:38:37 +0900

    LED 개수 수정

--- 12_debuging.ino
+++ 12_debuging.ino
@@ -8,7 +8,7 @@
 int cds = A0;
 
 //데이터 공간
-int time[LED_NUM] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+int time[LED_NUM] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 int temp[LED_NUM];
 
 //LED 지정
